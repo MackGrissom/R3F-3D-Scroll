9dae48c66f24079b8d350478e8f8f88d422d566f
Mack Grissom <mcintyregrissom@gmail.com>
2023-02-21 08:13:21 -0500

    added gsap animations to each floor

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {Canvas} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Experience } from './components/Experience'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
- <Canvas>
-  <Experience />
- </Canvas>
+    <Canvas camera={{
+      fov: 64,
+      position: [2.3, 1.5, 2.3],
+    }}>
+
+      <Experience />
+
+    </Canvas>
   )
 }
 
